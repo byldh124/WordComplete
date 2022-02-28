ed53c90113505bca7075a0529d654b0a9aa4174c
byldh <byldh@logibros.com>
2022-02-28 18:09:42 +0900

    버튼 애니메이션 추가

--- app/src/main/java/com/moondroid/wordcomplete/MainActivity.java
+++ app/src/main/java/com/moondroid/wordcomplete/MainActivity.java
@@ -2,7 +2,7 @@ package com.moondroid.wordcomplete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -10,14 +10,14 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int delay = 100;
 
     ImageView image, answerImage;
     TextView text;
@@ -42,10 +42,21 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Integer> stageGo = new ArrayList<>();
 
+    public static boolean sound = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = getSharedPreferences("Sound", MODE_PRIVATE);
+        sound = preferences.getBoolean("sound", true);
+        ImageView icSound = findViewById(R.id.icSound);
+        if (sound) {
+            icSound.setImageResource(R.drawable.ic_baseline_music_note_24);
+        } else {
+            icSound.setImageResource(R.drawable.ic_baseline_music_off_24);
+        }
 
         shake = AnimationUtils.loadAnimation(this, R.anim.shakeanimation);
         answerImage = findViewById(R.id.answer);
@@ -65,411 +76,119 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                start.setVisibility(View.GONE);
-
-                image.setImageResource(R.drawable.image001 + stageGo.get(stageGoNum));
-
-                str = word.get(stageGo.get(stageGoNum));
-
-                for (int i = 0; i < str.length(); i++) {
-                    btn.add((int) (str.charAt(i)));
-                }
-
-                Collections.shuffle(btn);
-
-                if (str.length() <= 5) {
-                    for (int i = 0; i < str.length(); i++) {
-                        alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
-                        alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
-                    }
-                } else {
-                    for (int i = 0; i < str.length() / 2; i++) {
-                        alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
-                        alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
-                    }
-
-                    int minus = str.length() - str.length() / 2;
-                    for (int i = 0; i < minus; i++) {
-                        alphabet[5 + i].setImageResource(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
-                        alphabet[5 + i].setTag(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
-                    }
-
-                }
-
-                btn.clear();
-
-
+                v.postDelayed(() -> {
+                    start.setVisibility(View.GONE);
+                    btnRset();
+                }, delay);
             }
         });
     }
 
-    public void click(View v) {
-        num++;
-        v.setVisibility(View.INVISIBLE);
-        char a = (char) ((int) (v.getTag()) - R.drawable.alphabet_a + 97);
-        answer = text.getText().toString();
-        answer += a;
-        text.setText(answer);
+    public void btnRset() {
+        image.setImageResource(R.drawable.image001 + stageGo.get(stageGoNum));
 
-        if (num == word.get(stageGo.get(stageGoNum)).length()) {
-            num = 0;
-            for (int i = 0; i < alphabet.length; i++) {
+        str = word.get(stageGo.get(stageGoNum));
+
+        for (int i = 0; i < str.length(); i++) {
+            btn.add((int) (str.charAt(i)));
+        }
+
+        Collections.shuffle(btn);
+
+        for (ImageView view : alphabet) {
+            view.setVisibility(View.GONE);
+        }
+
+        if (str.length() <= 5) {
+            for (int i = 0; i < str.length(); i++) {
                 alphabet[i].setVisibility(View.VISIBLE);
-                alphabet[i].setImageResource(0);
-
+                alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
+                alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
+            }
+        } else {
+            for (int i = 0; i < str.length() / 2; i++) {
+                alphabet[i].setVisibility(View.VISIBLE);
+                alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
+                alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
             }
 
-            if (answer.equals(word.get(stageGo.get(stageGoNum)))) {
+            int minus = str.length() - str.length() / 2;
+            for (int i = 0; i < minus; i++) {
+                alphabet[5 + i].setVisibility(View.VISIBLE);
+                alphabet[5 + i].setImageResource(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
+                alphabet[5 + i].setTag(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
+            }
+
+        }
+
+        btn.clear();
+    }
+
+    public void click(View v) {
+        v.postDelayed(() -> {
+            num++;
+            v.setVisibility(View.INVISIBLE);
+            char a = (char) ((int) (v.getTag()) - R.drawable.alphabet_a + 97);
+            answer = text.getText().toString();
+            answer += a;
+            text.setText(answer);
+
+            if (num == word.get(stageGo.get(stageGoNum)).length()) {
+                num = 0;
+                for (int i = 0; i < alphabet.length; i++) {
+                    alphabet[i].setVisibility(View.VISIBLE);
+                    alphabet[i].setImageResource(0);
+
+                }
+
+                if (answer.equals(word.get(stageGo.get(stageGoNum)))) {
 //                Toast.makeText(MainActivity.this, "정답입니다.", Toast.LENGTH_SHORT).show();
-                answerImage.setImageResource(R.drawable.answer_o);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        text.setText("");
-                        answerImage.setImageResource(0);
+                    answerImage.setImageResource(R.drawable.answer_o);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText("");
+                            answerImage.setImageResource(0);
 
-                        stageGoNum++;
+                            stageGoNum++;
 
-                        if (stageGoNum >= STAGE) {
-                            stageGoNum = 0;
-                            Collections.shuffle(stageGo);
-                        }
-
-
-                        image.setImageResource(R.drawable.image001 + stageGo.get(stageGoNum));
-
-                        str = word.get(stageGo.get(stageGoNum));
-
-                        for (int i = 0; i < str.length(); i++) {
-                            btn.add((int) (str.charAt(i)));
-                        }
-
-                        Collections.shuffle(btn);
-
-                        if (str.length() <= 5) {
-                            for (int i = 0; i < str.length(); i++) {
-                                alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
-                                alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
+                            if (stageGoNum >= STAGE) {
+                                stageGoNum = 0;
+                                Collections.shuffle(stageGo);
                             }
-                        } else {
-                            for (int i = 0; i < str.length() / 2; i++) {
-                                alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
-                                alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
-                            }
-
-                            int minus = str.length() - str.length() / 2;
-                            for (int i = 0; i < minus; i++) {
-                                alphabet[5 + i].setImageResource(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
-                                alphabet[5 + i].setTag(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
-                            }
-
+                            btnRset();
                         }
+                    }, 300);
 
-                        btn.clear();
-                    }
-                }, 300);
-
-            } else {
-                image.startAnimation(shake); //틀렸을때 애니메이션 흔들리게 하기
-                answerImage.setImageResource(R.drawable.answer_x);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        text.setText("");
-                        answerImage.setImageResource(0);
-                    }
-                }, 300);
+                } else {
+                    image.startAnimation(shake); //틀렸을때 애니메이션 흔들리게 하기
+                    answerImage.setImageResource(R.drawable.answer_x);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText("");
+                            answerImage.setImageResource(0);
+                        }
+                    }, 300);
 
 //                Toast.makeText(MainActivity.this, "틀렸습니다.", Toast.LENGTH_SHORT).show();
 
-                str = word.get(stageGo.get(stageGoNum));
-
-                for (int i = 0; i < str.length(); i++) {
-                    btn.add((int) (str.charAt(i)));
+                    btnRset();
                 }
-
-                Collections.shuffle(btn);
-
-                if (str.length() <= 5) {
-                    for (int i = 0; i < str.length(); i++) {
-                        alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
-                        alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
-                    }
-                } else {
-                    for (int i = 0; i < str.length() / 2; i++) {
-                        alphabet[i].setImageResource(R.drawable.alphabet_a + btn.get(i) - 97);
-                        alphabet[i].setTag(R.drawable.alphabet_a + btn.get(i) - 97);
-                    }
-
-                    int minus = str.length() - str.length() / 2;
-                    for (int i = 0; i < minus; i++) {
-                        alphabet[5 + i].setImageResource(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
-                        alphabet[5 + i].setTag(R.drawable.alphabet_a + btn.get(i + str.length() / 2) - 97);
-                    }
-
-                }
-                btn.clear();
             }
-        }
+        }, delay);
     }
 
-    public void wordSet() {
-        word.add("apple");
-        word.add("orange");
-        word.add("card");
-        word.add("sandwich");
-        word.add("painter");
-        word.add("doctor");
-        word.add("guitar");
-        word.add("television");
-        word.add("music");
-        word.add("clock");
-        word.add("icecream");
-        word.add("bubble");
-        word.add("sleep");
-        word.add("hamburger");
-        word.add("pizza");
-        word.add("spoon");
-        word.add("cook");
-        word.add("pilot");
-        word.add("nurse");
-        word.add("korea");
-        word.add("china");
-        word.add("japan");
-        word.add("america");
-        word.add("soccer");
-        word.add("basketball");
-        word.add("baseball");
-        word.add("swimming");
-        word.add("magic");
-        word.add("drum");
-        word.add("trumpet");
-        word.add("pig");
-        word.add("lion");
-        word.add("tiger");
-        word.add("hippo");
-        word.add("hedgehog");
-        word.add("prince");
-        word.add("princess");
-        word.add("king");
-        word.add("flower");
-        word.add("piano");
-        word.add("dog");
-        word.add("cat");
-        word.add("developer");
-        word.add("computer");
-        word.add("door");
-        word.add("deer");
-        word.add("window");
-        word.add("rabbit");
-        word.add("sheep");
-        word.add("horse");
-        word.add("monkey");
-        word.add("mouse");
-        word.add("quiz");
-        word.add("pencil");
-        word.add("singer");
-        word.add("scissors");
-        word.add("stone");
-        word.add("water");
-        word.add("fire");
-        word.add("green");
-        word.add("red");
-        word.add("blue");
-        word.add("pink");
-        word.add("yellow");
-        word.add("cicada");
-        word.add("mosquito");
-        word.add("larva");
-        word.add("seven");
-        word.add("eight");
-        word.add("butterfly");
-        word.add("earth");
-        word.add("candle");
-        word.add("pumpkin");
-        word.add("jewel");
-        word.add("giraffe");
-        word.add("chopstick");
-        word.add("strawberry");
-        word.add("peach");
-        word.add("tangerine");
-        word.add("bicycle");
-        word.add("donut");
-        word.add("bread");
-        word.add("calendar");
-        word.add("cheese");
-        word.add("dolphin");
-        word.add("grape");
-        word.add("hospital");
-        word.add("jungle");
-        word.add("mirror");
-        word.add("mountain");
-        word.add("night");
-        word.add("student");
-        word.add("tennis");
-        word.add("thunder");
-        word.add("tomato");
-        word.add("umbrella");
-        word.add("xylophone");
-        word.add("elephant");
-        word.add("panda");
-        word.add("turtle");
-        word.add("persimmon");
-        word.add("acorn");
-        word.add("carrot");
-        word.add("radish");
-        word.add("cucumber");
-        word.add("onion");
-        word.add("airplane");
-        word.add("airport");
-        word.add("autumn");
-        word.add("baby");
-        word.add("bag");
-        word.add("ball");
-        word.add("balloon");
-        word.add("banana");
-        word.add("bank");
-        word.add("basket");
-        word.add("bath");
-        word.add("bear");
-        word.add("beach");
-        word.add("bed");
-        word.add("bell");
-        word.add("bench");
-        word.add("book");
-        word.add("cake");
-        word.add("chair");
-        word.add("church");
-        word.add("coffee");
-        word.add("coin");
-        word.add("film");
-        word.add("fish");
-        word.add("flag");
-        word.add("glove");
-        word.add("hair");
-        word.add("hand");
-        word.add("handle");
-        word.add("heavy");
-        word.add("house");
-        word.add("island");
-        word.add("juice");
-        word.add("jump");
-        word.add("key");
-        word.add("knife");
-        word.add("lake");
-        word.add("letter");
-        word.add("leaf");
-        word.add("light");
-        word.add("marriage");
-        word.add("medal");
-        word.add("melon");
-        word.add("milk");
-        word.add("family");
-        word.add("moon");
-        word.add("sun");
-        word.add("money");
-        word.add("movie");
-        word.add("paint");
-        word.add("pants");
-        word.add("police");
-        word.add("potato");
-        word.add("rainbow");
-        word.add("ribbon");
-        word.add("rice");
-        word.add("rocket");
-        word.add("road");
-        word.add("school");
-        word.add("soap");
-        word.add("smile");
-        word.add("star");
-        word.add("station");
-        word.add("stove");
-        word.add("table");
-        word.add("swing");
-        word.add("tooth");
-        word.add("wind");
-        word.add("blowfish");
-        word.add("whale");
-        word.add("shark");
-        word.add("slipper");
-        word.add("socks");
-        word.add("hat");
-        word.add("castle");
-        word.add("mailbox");
-        word.add("box");
-        word.add("mole");
-        word.add("bat");
-        word.add("owl");
-        word.add("squirrel");
-        word.add("bottle");
-        word.add("penguin");
-        word.add("eggplant");
-        word.add("tree");
-        word.add("cloud");
-        word.add("sword");
-        word.add("thumb");
-        word.add("heart");
-        word.add("bulb");
-        word.add("teacher");
-        word.add("lollipop");
-        word.add("ship");
-        word.add("crown");
-        word.add("snow");
-        word.add("chick");
-        word.add("telephone");
-        word.add("accident");
-        word.add("alarm");
-        word.add("angel");
-        word.add("army");
-        word.add("atom");
-        word.add("bat");
-        word.add("bean");
-        word.add("bee");
-        word.add("beer");
-        word.add("beggar");
-        word.add("belt");
-        word.add("bill");
-        word.add("captain");
-        word.add("cart");
-        word.add("clock");
-        word.add("cold");
-        word.add("comb");
-        word.add("concert");
-        word.add("cookie");
-        word.add("corn");
-        word.add("cotton");
-        word.add("couple");
-        word.add("dawn");
-        word.add("desert");
-        word.add("dessert");
-        word.add("detective");
-        word.add("devil");
-        word.add("eraser");
-        word.add("exhibition");
-        word.add("exit");
-        word.add("factory");
-        word.add("fairy");
-        word.add("fall");
-        word.add("fist");
-        word.add("flute");
-        word.add("forest");
-        word.add("frog");
-        word.add("general");
-        word.add("ghost");
-        word.add("god");
-        word.add("hawk");
-        word.add("helicopter");
-        word.add("hurray");
-        word.add("iceberg");
-        word.add("kite");
-        word.add("lock");
-        word.add("meteor");
-        word.add("planet");
-        word.add("promise");
-        word.add("rhinoceros");
-        word.add("thief");
-        word.add("vote");
-        word.add("washer");
-        word.add("wolf");
+    public void changeSound(View view) {
+        AnimImageView iv = (AnimImageView) view;
+        if (sound) {
+            sound = false;
+            iv.setImageResource(R.drawable.ic_baseline_music_off_24);
+        } else {
+            sound = true;
+            iv.setImageResource(R.drawable.ic_baseline_music_note_24);
+        }
+        SharedPreferences preferences = getSharedPreferences("Sound", MODE_PRIVATE);
+        preferences.edit().putBoolean("sound", sound).commit();
     }
 }
