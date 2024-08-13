8c8f4774cd8f43abbf38ed9b41c8c558fa3818c5
byldh <byldh@sehanrf.co.kr>
2024-08-13 09:35:34 +0900

    sdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,11 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
 
+    //navigation
+    //implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    //implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    //implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.7")
+
     // dagger hilt
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-android-compiler:2.48")
